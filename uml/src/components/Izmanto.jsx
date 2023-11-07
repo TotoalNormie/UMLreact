06e46bcf5343d0a424eb937f1365ce49d3749002
ipb21.t.rudzitis <ipb21.t.rudzitis@pjvt.local>
2023-11-07 13:23:46 +0200

    Divi jaut pabeigti

---
+++ uml/src/components/Izmanto.jsx
@@ -0,0 +1,19 @@
+function Izmanto() {
+
+    return (
+      <>
+       <article>
+               <h2>ER diagrammas tiek izmantotas, lai:</h2>
+               <p>Plānotu datu bāzu struktūru un organizāciju.</p>
+               <p>Paredzētu, kā dati tiks glabāti un organizēti.</p>
+               <p>Vizuāli attēlotu, kā dati tiek iegūti un pārvaldīti.</p>
+               <p>Palīdzētu attīstītājiem un datu bāzu administrātoriem saprast datu modeli un attiecības starp datu vienībām.</p>
+
+            <p>Papildus info: ER diagrammas ir svarīga rīka datu bāzu projektēšanai un uzlabošanai, jo tās nodrošina skaidru attēlu par datu struktūru un attiecībām, kas var palīdzēt organizācijām efektīvi pārvaldīt un izmantot savus datus.</p>
+       </article>
+
+      </>
+    )
+  }
+  
+  export default Izmanto
