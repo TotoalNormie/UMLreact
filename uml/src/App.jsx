e1b74f37538c260facc8ccdb1a1ef23475d8d64f
ipb21.a.osins <ipb21.a.osins@pjvt.local>
2023-11-07 13:24:51 +0200

    article

--- uml/src/App.jsx
+++ uml/src/App.jsx
@@ -1,9 +1,13 @@
+import Article1 from './components/Article1';
+import Article2 from './components/Article2';
+
 function App() {
 
   return (
     <>
       <main>
-        test
+        <Article1></Article1>
+        <Article2></Article2>
       </main>
     </>
   )
