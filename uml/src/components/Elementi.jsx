a9f6b6d39f7d3f8cf8aeb74d8ea254724a7b8efc
ipb21.a.osins <ipb21.a.osins@pjvt.local>
2023-11-08 08:56:17 +0200

    upgrade

--- uml/src/components/Elementi.jsx
+++ uml/src/components/Elementi.jsx
@@ -4,9 +4,9 @@ function Elementi() {
       <>
         <article>
             <h2>ER diagrammas sastāv no vairākiem UML elementiem, tai skaitā:</h2>
-            <p>Entitāšu (datu vienību) attēli: Parāda dažādas datu vienības, piemēram, cilvēkus, produktus, pasūtījumus utt.</p>
-            <p>Attiecības: Parāda, kā dažādas datu vienības ir saistītas un kā tās mijiedarbojas vienā sistēmā.</p>
-            <p>Atribūti: Norāda datu vienību īpašības vai īpašību sarakstus.</p>
+            <p><strong>Entitāšu (datu vienību) attēli:</strong> Parāda dažādas datu vienības, piemēram, cilvēkus, produktus, pasūtījumus utt.</p>
+            <p><strong>Attiecības:</strong> Parāda, kā dažādas datu vienības ir saistītas un kā tās mijiedarbojas vienā sistēmā.</p>
+            <p><strong>Atribūti:</strong> Norāda datu vienību īpašības vai īpašību sarakstus.</p>
         </article>
       </>
     )
