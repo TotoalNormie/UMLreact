350d80e85af141a8f4e68d4b98826d4e03cbed73
Vitaly Juno <vvjuhno@gmail.com>
2023-11-08 09:35:57 +0200

    style changes

--- uml/src/components/Stuktura.jsx
+++ uml/src/components/Stuktura.jsx
@@ -1,16 +1,24 @@
-function Stuktura() {
+import diagram from '../../public/Diagram.png';
 
-    return (
-      <>
-        <article>
-          <h2>Kas tā ir un vai tā ir struktūras vai uzvedības modeļa daļa?</h2>
-          <p>Entity Relationship (ER) diagram ir datu modeļa daļa, kas tiek izmantota, 
-              lai attēlotu datu struktūru un attiecības starp dažādām datu vienībām sistēmā vai datu bāzē. 
-              Tas ir viens no visbiežāk izmantotajiem rīkiem datu bāzu projektēšanai un plānošanai, jo tas palīdz skaidri parādīt, kā dati ir saistīti un organizēti. 
-              ER diagramma var ietvert dažādas komponentes, kas attēlo datu struktūru un uzvedību.</p>
-        </article>
-      </>
-    )
-  }
-  
-  export default Stuktura
+function Stuktura() {
+	return (
+		<>
+			<article>
+				<h2>Kas tā ir un vai tā ir struktūras vai uzvedības modeļa daļa?</h2>
+				<p>
+					Entity Relationship (ER) diagramma ir datu modeļa daļa, kas tiek izmantota, lai
+					attēlotu datu struktūru un attiecības starp dažādām datu vienībām sistēmā vai
+					datubāzē. Tas ir viens no visbiežāk izmantotajiem rīkiem datubāzu projektēšanai
+					un plānošanai, jo tas palīdz skaidri parādīt, kā dati ir saistīti un organizēti.
+					ER diagramma var ietvert dažādas komponentes, kas attēlo datu struktūru un
+					uzvedību.
+				</p>
+				<div className='img'>
+					<img src={diagram} alt='Diagramma' />
+				</div>
+			</article>
+		</>
+	);
+}
+
+export default Stuktura;
