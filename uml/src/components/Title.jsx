f2f910cfe8e957e67cfae0ce7810714a20b24ba7
ipb21.a.osins <ipb21.a.osins@pjvt.local>
2023-11-08 10:24:51 +0200

    Title

---
+++ uml/src/components/Title.jsx
@@ -0,0 +1,15 @@
+function Title() {
+
+    return (
+      <>
+        <article>
+            <div className = "cont">
+                <h1>Entity Relationship (ER) diagramma</h1>
+                <p>Aleksis Ošiņš, Vitalijs Juhno IPb21</p>
+            </div>
+        </article>
+      </>
+    )
+  }
+  
+  export default Title
