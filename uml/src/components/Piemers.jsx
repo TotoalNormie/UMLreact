a53131c39527f12e53a4e299c08a24c2f3fec4db
Vitaly Juno <vvjuhno@gmail.com>
2023-11-08 09:08:21 +0200

    jkv

--- uml/src/components/Piemers.jsx
+++ uml/src/components/Piemers.jsx
@@ -1,20 +1,27 @@
 function Piemers() {
+	return (
+		<>
+			<article>
+				<h2>Piemērs ar skaidrojumu:</h2>
+				<div className='entity'>
+					<h3>Lietotāji (Users)</h3>
+					<p>
+						<strong>Lietotāja ID (User ID):</strong> unikāls identifikators katram
+						lietotājam.
+					</p>
+					<p>
+						<strong>Vārds (First Name):</strong> lietotāja vārds.
+					</p>
+					<p>
+						<strong>Uzvārds (Last Name):</strong> lietotāja uzvārds.
+					</p>
+					<p>
+						<strong>Epasta adrese (Email):</strong> lietotāja epasta adrese.
+					</p>
+				</div>
+			</article>
+		</>
+	);
+}
 
-    return (
-      <>
-       <article>
-        <h1>Piemērs ar skaidrojumu:</h1>
-       <div class="entity">
-        <h2>Lietotāji (Users)</h2>
-        <p><strong>Lietotāja ID (User ID):</strong> unikāls identifikators katram lietotājam.</p>
-        <p><strong>Vārds (First Name):</strong> lietotāja vārds.</p>
-        <p><strong>Uzvārds (Last Name):</strong> lietotāja uzvārds.</p>
-        <p><strong>Epasta adrese (Email):</strong> lietotāja epasta adrese.</p>
-    </div>
-       </article>
-
-      </>
-    )
-  }
-  
-  export default Piemers
+export default Piemers;
