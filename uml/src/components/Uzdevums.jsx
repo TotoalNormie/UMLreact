55366fd7334b8631478c4cb77cfb9148cbd2629c
Vitaly Juno <vvjuhno@gmail.com>
2023-11-08 10:05:58 +0200

    ubio

--- uml/src/components/Uzdevums.jsx
+++ uml/src/components/Uzdevums.jsx
@@ -3,32 +3,35 @@ function Uzdevums() {
 		<>
 			<article>
 				<h2>Uzdevums</h2>
-                <p>Dodies uz <a href="draw.io">draw.io</a> un izveido šādu ER Diagramu:</p>
-                <p>Klienti (tabula 1)</p>
+				<p>
+					Dodies uz <a href='draw.io'>draw.io</a> un izveido šādu ER Diagramu:
+				</p>
+				<div className='wrapper'>
+					<ul>
+						<strong>Klienti (tabula 1)</strong>
+						<li>Klienta_ID (Primārais atslēgas lauks)</li>
+						<li>Vārds (teksts)</li>
+						<li>Uzvārds (teksts)</li>
+						<li>E-pasts (teksts)</li>
+						<li>Telefona numurs (teksts)</li>
+						<li>Pasūtījumi (tabula 2)</li>
+					</ul>
 
-                <strong>Klienta_ID (Primārais atslēgas lauks)</strong>
-                <ul>
-                <li>Vārds (teksts)</li>
-                <li>Uzvārds (teksts)</li>
-                <li>E-pasts (teksts)</li>
-                <li>Telefona numurs (teksts)</li>
-                <li>Pasūtījumi (tabula 2)</li>
-                </ul>
+					<ul>
+						<strong>Pasūtījuma_ID (Primārais atslēgas lauks)</strong>
+						<li>Pasūtījuma datums (datums)</li>
+						<li>Pasūtījuma summa (decimālskaitlis)</li>
+						<li>Piegādes adrese (teksts)</li>
+					</ul>
 
-                <strong>Pasūtījuma_ID (Primārais atslēgas lauks)</strong>
-                <ul>
-                <li>Pasūtījuma datums (datums)</li>
-                <li>Pasūtījuma summa (decimālskaitlis)</li>
-                <li>Piegādes adrese (teksts)</li>
-                </ul>
-
-                <strong>Preces (tabula 3)</strong>
-                <ul>
-                <li>Preces_ID (Primārais atslēgas lauks)</li>
-                <li>Preces nosaukums (teksts)</li>
-                <li>Preces cena (decimālskaitlis)</li>
-                <li>Pieejamie daudzumi (veselie skaitļi)</li>
-                </ul>
+					<ul>
+						<strong>Preces (tabula 3)</strong>
+						<li>Preces_ID (Primārais atslēgas lauks)</li>
+						<li>Preces nosaukums (teksts)</li>
+						<li>Preces cena (decimālskaitlis)</li>
+						<li>Pieejamie daudzumi (veselie skaitļi)</li>
+					</ul>
+				</div>
 			</article>
 		</>
 	);
