de2e49edc783f90633901875dbebb7185bb28455
Vitaly Juno <vvjuhno@gmail.com>
2023-11-08 10:36:13 +0200

    sdv

--- uml/src/App.jsx
+++ uml/src/App.jsx
@@ -4,12 +4,14 @@ import Izmanto from './components/Izmanto'
 import Diagramma from './components/Diagramma'
 import Piemers from './components/Piemers'
 import Uzdevums from './components/Uzdevums'
+import Titlus from './components/Titlus'
 
 function App() {
 
   return (
     <>
       <main>
+        <Titlus />
         <Stuktura />
         <Izmanto />
         <Elementi />
